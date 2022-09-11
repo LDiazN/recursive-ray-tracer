8f91c5a3b2846163b8f11a7c89029cd4b44acd00
Luis Diaz <ldiazn98@gmail.com>
2022-09-11 12:54:19 -0400

    first fully working version of simple ray tracing with phong shading

--- rec_rays/src/RecursiveRayTracer.cpp
+++ rec_rays/src/RecursiveRayTracer.cpp
@@ -8,6 +8,8 @@
 // Vendor includes
 #include <glm/gtc/matrix_transform.hpp>
 #include <SDL.h>
+#include <threadpool.h>
+
 
 namespace RecRays
 {
@@ -64,7 +66,7 @@ namespace RecRays
 
 		// Set up normals
 		auto const inverseTransform = glm::transpose(glm::inverse(transform));
-		for(auto& normal : geometry.normals)
+		for (auto& normal : geometry.normals)
 		{
 			normal = glm::vec3(inverseTransform * glm::vec4(normal, 0.f));
 		}
@@ -120,10 +122,10 @@ namespace RecRays
 
 
 		glm::mat4 inverseTranslation = {
-			0.f, 0.f, 0.f, 0.f,
-			0.f, 0.f, 0.f, 0.f,
-			0.f, 0.f, 0.f, 0.f,
-			-(m_Position.x - m_PosToLookAt.x), -(m_Position.y - m_PosToLookAt.y), -(m_Position.z - m_PosToLookAt.z), 0.f,
+			1.f, 0.f, 0.f, 0.f,
+			0.f, 1.f, 0.f, 0.f,
+			0.f, 0.f, 1.f, 0.f,
+			-(m_Position.x - m_PosToLookAt.x), -(m_Position.y - m_PosToLookAt.y), -(m_Position.z - m_PosToLookAt.z), 1.f,
 		};
 
 		return axisTransform * inverseTranslation;
@@ -132,10 +134,10 @@ namespace RecRays
 
 	// -- < Ray Generation > ----------------------------------------------
 
-	Ray RayGenerator::GetRayThroughPixel(size_t pixelX, size_t pixelY, RayType type)
+	Ray RayGenerator::GetRayThroughPixel(size_t pixelX, size_t pixelY, RayType type) const
 	{
 		// Find the top left corner to get position of next pixel from this
-		glm::vec3 topLeftCorner = -m_Left * m_Camera.GetU() + m_Top * m_Camera.GetW();
+		glm::vec3 topLeftCorner = -m_Left * m_Camera.GetU() + m_Top * m_Camera.GetW() + m_DistanceToViewPlane * m_Camera.GetV();
 
 		// Use pixel width and height to find how much to offset for each step
 		float pixelWidth = (m_Right + m_Left) / static_cast<float>(m_PixelsX);
@@ -145,16 +147,16 @@ namespace RecRays
 		float verticalOffset = static_cast<float>(pixelY) * pixelHeight;
 
 		// Use offset in camera coordinates to find how much in each direction to move
-		auto pixelCoordinates = topLeftCorner + 
-								m_Camera.GetU() * horizontalOffset - 
-								m_Camera.GetW() * verticalOffset;
+		auto pixelCoordinates = topLeftCorner +
+			m_Camera.GetU() * horizontalOffset -
+			m_Camera.GetW() * verticalOffset;
 
 		// Now depending on the ray type, we find a different position inside the pixel
-		glm::vec3 offsetInsidePixel;
+		glm::vec3 offsetInsidePixel(0);
 
 		switch (type)
 		{
-		case RayType::TOP_LEFT: offsetInsidePixel = glm::vec3(0); break;
+		case RayType::TOP_LEFT: break;
 		case RayType::MID: offsetInsidePixel = m_Camera.GetU() * pixelWidth / 2.f - m_Camera.GetW() * pixelHeight / 2.f; break;
 		default: assert(false && "Ray type not yet implemented");
 		}
@@ -186,11 +188,11 @@ namespace RecRays
 
 	}
 
-	int RecursiveRayTracer::Draw(FIBITMAP*& outImage)
+	int RecursiveRayTracer::Draw(FIBITMAP*& outImage, size_t nThreads = 12)
 	{
 		// Allocate space for this image
 		auto Image = FreeImage_Allocate(
-			m_SceneDescription.imgResX, 
+			m_SceneDescription.imgResX,
 			m_SceneDescription.imgResY,
 			static_cast<size_t>(8 * 3)); // 8 bit colors, 3 color components
 
@@ -201,6 +203,27 @@ namespace RecRays
 		// enough to simulate camera positioning
 		SetUpGeometry();
 
+		// Concurrency stuff: Render disjoint segments of the screen in multiple threads
+		thread_pool threads(nThreads);
+		std::vector<std::future<void>> futures;
+
+		// Where the colors are actually drawn
+		TwoDimensionVector<glm::vec4> colorBuffer(m_SceneDescription.imgResX, m_SceneDescription.imgResY);
+
+		// Start parallel shading: Schedule regions for each thread
+		uint32_t const yIntervalSize = m_SceneDescription.imgResY / nThreads;
+		for (uint32_t i = 0; i < nThreads; i++)
+		{
+			futures.push_back(threads.execute(&RecursiveRayTracer::DrawThread, this, std::ref(colorBuffer), 0, m_SceneDescription.imgResX, i * yIntervalSize, (i + 1) * yIntervalSize));
+		}
+
+		// TODO finish parallel shading. You schedule a way to display results before they're finished using SDL
+
+		// Now we can intersect things with rays
+		RGBQUAD color;
+		// Use this variables to print a progress bar
+
+		// Draw in SDL while futures are not yet done 
 		// Set up SDL window
 		SDL_Renderer* renderer = nullptr;
 		SDL_Window* window = nullptr;
@@ -210,55 +233,73 @@ namespace RecRays
 		SDL_RenderClear(renderer);
 		SDL_SetWindowTitle(window, "Ray Tracer preview");
 
-		// Now we can intersect things with rays
-		RGBQUAD color;
-		ProgressBar bar(m_SceneDescription.imgResX * m_SceneDescription.imgResY);
-		// Use this variables to print a progress bar
-		
+		bool ready = false;
+
+		while (!ready)
+		{
+			for (size_t i = 0; i < m_SceneDescription.imgResY; i++)
+			{
+				for (size_t j = 0; j < m_SceneDescription.imgResX; j++)
+				{
+					glm::vec4 shadeColor = 255.0f * colorBuffer.Get(i, j);
+					shadeColor.r = glm::clamp(shadeColor.r, 0.f, 255.f);
+					shadeColor.g = glm::clamp(shadeColor.g, 0.f, 255.f);
+					shadeColor.b = glm::clamp(shadeColor.b, 0.f, 255.f);
+
+					// Update renderer & window
+					SDL_SetRenderDrawColor(
+						renderer,
+						static_cast<Uint8>(shadeColor.r),
+						static_cast<Uint8>(shadeColor.g),
+						static_cast<Uint8>(shadeColor.b),
+						255
+					);
+
+					SDL_RenderDrawPoint(renderer, i, j);
+
+					// Update bar
+				}
+			}
+
+			if (SDL_PollEvent(&event)); // do nothing with event
+
+			// Display image
+			SDL_RenderPresent(renderer);
+
+			// Update ready status
+			bool allEnded = true;
+			for (auto const& future : futures)
+			{
+				if (future.wait_for(std::chrono::seconds(0)) != std::future_status::ready)
+				{
+					allEnded = false;
+					break;
+				}
+			}
+			ready = allEnded;
+		}
+
+		// Draw final FreeImage output image
 		for (size_t i = 0; i < m_SceneDescription.imgResY; i++)
 		{
 			for (size_t j = 0; j < m_SceneDescription.imgResX; j++)
 			{
-				// Set next pixel to render as white
-				SDL_SetRenderDrawColor(renderer, 1, 1, 1, 1);
-				SDL_RenderDrawPoint(renderer, i, j);
-				SDL_RenderPresent(renderer);
+				glm::vec4 shadeColor = 255.0f * colorBuffer.Get(i, j);
 
-				auto const ray = m_RayGenerator.GetRayThroughPixel(i, j);
-				auto result = IntersectRay(ray);
+				shadeColor.r = glm::clamp(shadeColor.r, 0.f, 255.f);
+				shadeColor.g = glm::clamp(shadeColor.g, 0.f, 255.f);
+				shadeColor.b = glm::clamp(shadeColor.b, 0.f, 255.f);
 
-				auto const shadeColor = 255.0f * Shade(result);
-
-				// -- DEBUG ONLY ------------------------
-				//glm::vec3 const shadeColor(255.0f * glm::dot(ray.direction, m_RayGenerator.GetCamera().GetV()));
-			
-				// --------------------------------------
 				color.rgbRed = shadeColor.r;
 				color.rgbGreen = shadeColor.g;
 				color.rgbBlue = shadeColor.b;
-				// Write to image
+
 				FreeImage_SetPixelColor(Image, i, j, &color);
-
-				// Update renderer & window
-				SDL_SetRenderDrawColor(
-					renderer, 
-					static_cast<Uint8>(shadeColor.r), 
-					static_cast<Uint8>(shadeColor.g), 
-					static_cast<Uint8>(shadeColor.b), 
-					static_cast<Uint8>(shadeColor.a)
-				);
-
-				SDL_RenderDrawPoint(renderer, i, j);
-
-				if (SDL_PollEvent(&event));
-
-				SDL_RenderPresent(renderer);
-
-				// Update bar
-				bar.Step();
-				bar.Draw();
 			}
 		}
+
+		for (auto& future : futures)
+			future.get(); // end all threads
 
 		SDL_DestroyRenderer(renderer);
 		SDL_DestroyWindow(window);
@@ -267,13 +308,29 @@ namespace RecRays
 		return SUCCESS;
 	}
 
+	void RecursiveRayTracer::DrawThread(TwoDimensionVector<glm::vec4>& outBuffer, size_t startI, size_t endI, size_t startJ, size_t endJ)
+	{
+		for (size_t i = startI; i < endI; i++)
+		{
+			for (size_t j = startJ; j < endJ; j++)
+			{
+
+				auto const ray = m_RayGenerator.GetRayThroughPixel(i, j);
+				auto const result = IntersectRay(ray);
+				auto shadeColor = Shade(result);
+
+				outBuffer.Set(i, j, shadeColor);
+			}
+		}
+	}
+
 	float RecursiveRayTracer::FocalLength(float fovy, float height)
 	{
 		float const cos = glm::pow(glm::cos(fovy / 2.f), 2.f);
 		float result = cos * height * height;
 		result = result / (1 - cos);
 		result = glm::sqrt(result);
-		
+
 		return result;
 	}
 
@@ -295,7 +352,7 @@ namespace RecRays
 		// Find nearest object intersecting this ray
 		RayIntersectionResult finalResult;
 		float nearestT = INFINITY;
-		for (auto const &obj : m_SceneDescription.GetObjects())
+		for (auto const& obj : m_SceneDescription.GetObjects())
 		{
 			auto const result = IntersectRayToObject(ray, obj, 0, nearestT);
 			if (result.WasIntersection() && result.t < nearestT && result.t > 0)
@@ -303,7 +360,7 @@ namespace RecRays
 				finalResult = result;
 				nearestT = result.t;
 			}
-			
+
 		}
 
 		return finalResult;
@@ -317,7 +374,7 @@ namespace RecRays
 			return IntersectRayToSphere(ray, object);
 		default:
 			return IntersectRayToTesselatedObject(ray, object, minT, maxT);
-		}	
+		}
 	}
 
 	RayIntersectionResult RecursiveRayTracer::IntersectRayToSphere(const Ray& ray, const Object& sphere, float minT, float maxT) const
@@ -325,13 +382,13 @@ namespace RecRays
 		// Sanity check 
 		assert(sphere.shape == Shape::Sphere);
 
-		// We just have to compue intersection point solving for t in the
-		// ecuation of a sphere substituting by a point in the ray
+		// We just have to compute intersection point solving for t in the
+		// equation of a sphere substituting by a point in the ray
 
 		// Compute discriminant to check what kind of intersection we have here
 		auto const d = ray.direction;
 		auto const e = ray.position;
-		auto const c = glm::vec3 (sphere.transform * glm::vec4(0,0,0,1)); // Extract sphere position from transform
+		auto const c = glm::vec3(sphere.transform * glm::vec4(0, 0, 0, 1)); // Extract sphere position from transform
 		auto const r = sphere.size; // Radius
 
 		auto discriminant = glm::dot(d, e - c);
@@ -340,7 +397,7 @@ namespace RecRays
 
 		float t = -dot(d, e - c);
 		std::vector<float> results;
-		auto emptyResult = RayIntersectionResult{ nullptr, glm::vec3(0), glm::vec3(0), 0 };
+		auto emptyResult = RayIntersectionResult{ nullptr, glm::vec3(0), glm::vec3(0), 0 , ray };
 		if (discriminant < -0.0001)
 		{
 			// no intersection at all
@@ -372,52 +429,69 @@ namespace RecRays
 
 		// Compute intersection point and normal
 		glm::vec3 const intersectionPos = ray.position + desiredResult * ray.direction;
-		glm::vec3 const intersectionNormal = glm::vec3(intersectionPos - c);
+		glm::vec3 const intersectionNormal = glm::normalize(glm::vec3(intersectionPos - c));
 
 		return RayIntersectionResult{
 				std::make_shared<const Object*>(&sphere),
 				intersectionNormal,
-				intersectionPos, desiredResult };
+				intersectionPos, desiredResult, ray };
+	}
+
+	bool RecursiveRayTracer::PointInsideTriangle(const glm::vec3& point, const glm::vec3& v1, const glm::vec3& v2, const glm::vec3& v3)
+	{
+		auto const edge1 = v2 - v1;
+		auto const edge2 = v3 - v2;
+		auto const edge3 = v1 - v3;
+
+		auto const v12P = point - v1;
+		auto const v22P = point - v2;
+		auto const v32P = point - v3;
+
+		auto const normal = glm::cross(v2 - v1, v3 - v1);
+
+		return	glm::dot(normal, glm::cross(edge1, v12P)) > 0 &&
+				glm::dot(normal, glm::cross(edge2, v22P)) > 0 &&
+				glm::dot(normal, glm::cross(edge3, v32P)) > 0
+		;
 	}
 
 	bool  RecursiveRayTracer::IntersectRayToTriangle(const Ray& ray, const glm::vec3& v1, const glm::vec3& v2, const glm::vec3& v3, const glm::vec3& n1, const glm::vec3& n2, const glm::vec3& n3, glm::vec3& outIntersection, glm::vec3& outNormal, float& outT, float minT, float maxT)
 	{
 		// Before everything, perform backface culling. If direction and triangle normal
 		// have an angle > 90, then it can't intersect by any mean. Winding order is clock wise
-		auto const triangleNormal = glm::normalize(glm::cross(v2 - v1, v3 - v1));
+		auto const triangleNormal = glm::cross(v2 - v1, v3 - v1);
 		if (glm::dot(ray.direction, triangleNormal) >= 0)
 			return false;
 
-		// In this function we use the final result of solving the ec. system
-		// of intersecting the ray with the baricentric coordinates of the triangle
-		auto const detA = glm::determinant(glm::mat3(v1 - v2, v1 - v3, ray.direction));
+		constexpr float EPSILON = 0.0000001f;
 
-		auto const t = glm::determinant(glm::mat3(v1 - v2, v1 - v3, v1 - ray.position)) / detA;
+		auto const edge1 = v2 - v1;
+		auto const edge2 = v3 - v1;
+		auto const h = glm::cross(ray.direction, edge2);
+		auto const k = glm::dot(edge1, h);
 
-		// Check if intersection point is in valid range
-		if (t < minT || t > maxT)
+		// No intersection, ray is parallel to triangle
+		if (k > -EPSILON && k < EPSILON)
 			return false;
 
-		// Check if beta in valid range
-		auto const beta = glm::determinant(glm::mat3(v1 - v2, v1 - ray.position, ray.direction)) / detA;
-		if (beta < 0 || beta > 1)
+		auto const f = 1.f / k;
+		auto const s = ray.position - v1;
+		auto const u = f * glm::dot(s, h);
+		if (u < 0.0f || u > 1.0)
 			return false;
 
-		// Check if alpha in valid range
-		auto const alpha = glm::determinant(glm::mat3(v1 - ray.position, v1 - v3, ray.direction)) / detA;
-		if (alpha < 0 || alpha + beta > 1)
+		auto const q = glm::cross(s, edge1);
+		auto const v = f * glm::dot(ray.direction, q);
+		if (v < 0.0 || u + v > 1.0)
 			return false;
 
-		// Now that we actually hit the triangle, we need to compute the normals.
-		// We do so by interpolating the normals of the other 3 vertices.
-		// We have to find a,b,c such that
-		// intersectionPoint = a * v1 + b * v2 + c * v3
-		// and then we will use these numbers such that
-		// newNormal = a * n1 + b * n2 + c * n3.
-		// And therefore we have to solve for
-		// [v1 v2 v3] * [a b c]' = intersectionPoint
+		auto const t = f * glm::dot(edge2, q);
+		if (t <= EPSILON)
+			return false; // Line intersection, but not ray
 
-		auto const intersectionPoint = ray.position + t * ray.direction;
+		auto const intersectionPoint = ray.position + ray.direction * t;
+
+		// Compute normal based on intersection position
 		auto vecMatrix = glm::mat3(v1, v2, v3);
 		auto const vecMatrixDet = glm::determinant(vecMatrix);
 
@@ -451,6 +525,7 @@ namespace RecRays
 		// sanity check
 		assert(object.shape != Shape::Sphere && "Sphere is parametric object");
 
+
 		glm::vec3 intersection, normal;
 		float t = maxT;
 		bool hitSome = false;
@@ -474,9 +549,9 @@ namespace RecRays
 			if (!wasIntersection)
 				continue;
 
-			if (nextT > minT && nextT < t)
+			if (nextT >= minT && nextT < t)
 			{
-				hitSome = hitSome || wasIntersection;
+				hitSome = true;
 				t = nextT;
 				intersection = nextIntersect;
 				normal = nextNormal;
@@ -490,7 +565,9 @@ namespace RecRays
 			std::make_shared<const Object*>(&object),
 				normal,
 				intersection,
-				t };
+				t,
+				ray
+			};
 		}
 	
 		return RayIntersectionResult{nullptr, glm::vec3(0), glm::vec3(0), 0 };
@@ -498,12 +575,58 @@ namespace RecRays
 
 	glm::vec4 RecursiveRayTracer::Shade(const RayIntersectionResult& rayIntersection)
 	{
-
-		if (rayIntersection.WasIntersection())
-			//return (*rayIntersection.object)->diffuse;
-			return glm::vec4(1);
-		else
+		// If no intersection, do nothing and return black
+		if (!rayIntersection.WasIntersection())
 			return glm::vec4(0);
+
+
+		auto normal = glm::normalize(rayIntersection.normal);
+		glm::vec4 lightColor(0);
+		auto const &object = **rayIntersection.object;
+		// Add ambient color
+		lightColor += object.ambient;
+
+		// Compute diffuse + specular for each light
+		glm::vec4 diffuse(0), specular(0);
+		for (auto const & light : m_SceneDescription.GetLights())
+		{
+			// Compute direction of light. If point light, then use relative position.
+			// If directional light, use straight up as direction.
+			glm::vec3 lightDirection(0);
+			if (light.position.w == 0.0)
+				lightDirection = glm::normalize(light.position);
+			else
+				lightDirection = glm::normalize(glm::vec3(light.position) - rayIntersection.position);
+
+			// Compute diffuse 
+			diffuse +=
+				object.diffuse *
+				light.color *
+				glm::max(0.f, glm::dot(glm::vec3(normal), lightDirection));
+
+			// Compute specular
+			const glm::vec3 halfVec = glm::normalize(
+					lightDirection + 
+					(
+						-rayIntersection.ray.direction
+					)
+			);
+
+			specular += glm::pow(
+						glm::max(
+							0.f, 
+							glm::dot(glm::vec3(normal), halfVec)
+							), 
+						object.shininess
+						) * 
+						light.color * 
+						object.specular;
+
+		}
+
+		lightColor += diffuse + specular;
+		lightColor.a = 1;
+		return lightColor;
 	}
 
 	// -- <Progres bar> ------------------------------------------------------------------------------------------
@@ -522,7 +645,7 @@ namespace RecRays
 			else if (i == pos) std::cout << ">";
 			else std::cout << " ";
 		}
-		std::cout << "] " << static_cast<size_t>(progress * 100.0) <<  "(" << m_CurrentSteps << " / " << m_NSteps << ")" " %\r";
+		std::cout << "] " << static_cast<size_t>(progress * 100.0) <<  " %(" << m_CurrentSteps << " / " << m_NSteps << ")" "\r";
 		std::cout.flush();
 	}
 }
